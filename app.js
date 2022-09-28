503198b7b71811e13d81a05c7fa262a583ca7d42
Mihai <smolenschi.mihai@gmail.com>
2022-09-28 19:39:03 +0300

    timer feature work in progress

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 "use strict";
 
-const gameEl = document.getElementById("game");
+const boardEl = document.getElementById("board");
 const scoreEl = document.getElementById("score");
 const missEl = document.getElementById("miss");
 const targetsCurrentValueEl = document.getElementById("targets--value");
 const numberOfTargetsEl = document.getElementById("targets--number");
 const respawnValueEl = document.getElementById("respawn--value");
+
+// -- Toggle menu elements
+const navbarEl = document.querySelector(".navbar");
+const btnToggleMenu = document.querySelector(".btn-toggle-menu");
 
 const respawnCurrentValueEl = document.getElementById("respawn--current-value");
 
@@ -15,7 +19,8 @@ const scoreStatusEl = document.getElementById("score--status");
 
 const targets = [];
 let score = 0;
-let missCounter = 0;
+let hittedTargetsCounter = 0;
+let missedTargetsCounter = 0;
 let respawnTargetInterval;
 let targetRespawnTime = 1000; // milliseconds
 
@@ -28,35 +33,181 @@ targetsCurrentValueEl.max = 20;
 targetsCurrentValueEl.value = numberOfTargetsLimit;
 numberOfTargetsEl.innerText = numberOfTargetsLimit;
 
+// -- Target Size
+targetSizeEl.min = 20;
+targetSizeEl.max = 120;
+targetSizeEl.step = 1;
+targetSizeEl.value = targetWidth;
+
+const currentSizeValueEl = document.getElementById("size--current-value");
+currentSizeValueEl.innerText = targetWidth;
+
+// -- Respawn target
+respawnValueEl.min = 1;
+respawnValueEl.max = 2000;
+respawnValueEl.value = targetRespawnTime;
+respawnCurrentValueEl.innerText = targetRespawnTime;
+
+// *******************
+// -- Event Listeners
+// *******************
+
+// -- Increment/Decrement
+targetsCurrentValueEl.addEventListener("input", changeTargetsNumber);
+
+// -- Change Respawn time
+respawnValueEl.addEventListener("input", changeRespawnTime);
+
+// -- Change Target size
+targetSizeEl.addEventListener("input", setTargetWidth);
+
+// -- Change Toggle menu
+btnToggleMenu.addEventListener("click", () => {
+  navbarEl.classList.toggle("open-menu");
+});
+
+// ********************
+// -- Timer feature
+// ********************
+
+// ********************
+// -- TODO:
+// ********************
+// [+] Create 'new game' button
+//  [+] show it when game loads
+//  [] when pressed start the game
+// [] When timer out of time the game will:
+//  [] stop
+//  [] show a modal window
+//  [] with accuracy, target hits, and missed
+//  [] below will show the 'new game button'
+
+// ********************
+// -- New game button
+// ********************
+
+let startGame = false;
+let timer = 10;
+
+const timerEl = document.getElementById("timer");
+
+const newGameBtnEl = document.createElement("button");
+
+newGameBtnEl.innerText = "New Game";
+newGameBtnEl.classList.add("btn--new-game");
+newGameBtnEl.id = "new-game";
+
+boardEl.appendChild(newGameBtnEl);
+
+newGameBtnEl.addEventListener("click", () => {
+  startGame = true;
+
+  boardEl.innerHTML = "";
+
+  // boardEl.removeChild(newGameBtnEl);
+
+  const timerInterval = setInterval(() => {
+    timerEl.innerHTML = --timer;
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(timerInterval);
+    clearInterval(respawnTargetInterval);
+
+    boardEl.removeEventListener("mousedown", missedTargetLogic);
+    boardEl.removeEventListener("contextmenu", missedTargetLogic);
+
+    // remove all from board
+    boardEl.innerHTML = ``;
+
+    const resultEl = document.createElement("div");
+
+    resultEl.innerHTML = `
+      <div class="result-container">
+        <div>
+          <span>Score:</span><span>${score}</span>
+        </div>
+        <div>
+          <span>Hit:</span><span id="hit" class="result--hit">${hittedTargetsCounter}</span>
+        </div>
+        <div>
+          <span>Missed:</span>
+          <span id="missed" class="result--missed">${missedTargetsCounter}</span>
+        </div>
+        <div>
+          <span>Accurracy:</span>
+          <span id="accuracy" class="result--accuracy">50%</span>
+        </div>
+      </div>
+    `;
+
+    boardEl.appendChild(resultEl);
+
+    boardEl.appendChild(newGameBtnEl);
+
+    startGame = false;
+    timerEl.innerText = timer = 10;
+    missEl.innerText = missedTargetsCounter = 0;
+    scoreEl.innerText = score = 0;
+    updateScoreStatus();
+  }, 10000); // 10s
+
+  // -- Updates every "n" millisecons
+  respawnTargetInterval = setInterval(checkTargets, targetRespawnTime);
+
+  // -- Missed target left click
+  boardEl.addEventListener("mousedown", missedTargetLogic);
+
+  // -- Missed target right click
+  boardEl.addEventListener("contextmenu", missedTargetLogic);
+});
+
+// *******************
+// -- Functions
+// *******************
+const randomId = () => Math.random().toString();
+
 const randomNumberBetweenMinMax = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
-const randomCoordinate = (max) => {
+function randomCoordinate(max) {
   // to not get a random number above the limit of the board width/height
   const top = max - targetWidth;
   return Math.floor(Math.random() * top + 1);
-};
+}
 
-const randomId = () => Math.random().toString();
+function checkTargets() {
+  const numberOfTargets = boardEl.querySelectorAll(".target").length;
 
-const updateScore = (points) => {
-  if (points === 0) {
-    score = 0;
-
-    scoreEl.innerText = score;
-
-    updateScoreStatus();
-    return;
+  if (numberOfTargets < numberOfTargetsLimit) {
+    createTarget(
+      randomCoordinate(boardEl.offsetWidth),
+      randomCoordinate(boardEl.offsetHeight)
+    );
   }
 
-  score += points;
+  const targetsArray = boardEl.querySelectorAll(".target");
+  for (let i = 0; i < targetsArray.length; i++) {
+    changeTargetWidth(targetsArray[i]);
+    moveTargetOnTheBoard(targetsArray[i]);
+  }
+}
+
+function updateScore(points) {
+  if (points > 0) {
+    score += points;
+    updateScoreStatus("add", points);
+  }
+  if (points < 0) {
+    score -= -points;
+    updateScoreStatus("substract", points);
+  }
 
   scoreEl.innerText = score;
+}
 
-  updateScoreStatus("add", points);
-};
-
-function updateScoreStatus(className, score) {
+function updateScoreStatus(className, points) {
+  // if no class then remove and set score status to ''
   if (className === undefined) {
     scoreStatusEl.classList.remove("substract");
     scoreStatusEl.classList.remove("add");
@@ -67,18 +218,21 @@ function updateScoreStatus(className, score) {
   if (className === "add") {
     scoreStatusEl.classList.remove("substract");
     scoreStatusEl.classList.add("add");
-    scoreStatusEl.innerText = "+" + score;
-  } else if (className === "substract") {
+    scoreStatusEl.innerText = "+" + points;
+  }
+  if (className === "substract") {
     scoreStatusEl.classList.remove("add");
     scoreStatusEl.classList.add("substract");
-    scoreStatusEl.innerText = "-" + score;
+    scoreStatusEl.innerText = points;
   }
 }
 
 function targetLogic(target) {
   updateScore(target.value);
 
-  gameEl.removeChild(target);
+  boardEl.removeChild(target);
+
+  hittedTargetsCounter++;
 }
 
 function createTarget(x, y) {
@@ -89,77 +243,82 @@ function createTarget(x, y) {
 
   let currentPoints = Math.round(targetLifeSpan / 1000);
 
-  const losePoints = currentPoints;
+  const lostPoints = currentPoints;
 
   target.className = "target";
-  console.log(gameEl.offsetWidth);
 
   target.style.left = x + "px";
   target.style.top = y + "px";
 
   target.id = "target-" + randomId();
 
-  target.innerHTML = innerTargetHTML(currentPoints, losePoints);
+  target.innerHTML = innerTargetHTML(currentPoints, lostPoints);
 
   changeTargetWidth(target);
 
   target.value = currentPoints;
 
-  gameEl.appendChild(target);
+  boardEl.appendChild(target);
 
   setTimeout(() => {
-    if (gameEl.contains(target)) {
-      gameEl.removeChild(target);
+    if (boardEl.contains(target)) {
+      boardEl.removeChild(target);
 
       // if not clicked in time you lose points
-      countMisses(losePoints);
+      updateScore(-lostPoints);
+
+      missedTargetsCounter++;
+
+      missEl.innerText = missedTargetsCounter;
     }
   }, targetLifeSpan);
 
   setInterval(() => {
     target.value = --currentPoints;
 
-    target.innerHTML = innerTargetHTML(currentPoints, losePoints);
+    target.innerHTML = innerTargetHTML(currentPoints, lostPoints);
   }, 1000);
 
-  // Click and right click for targets
-
-  target.addEventListener("mousedown", (event) => {
+  // Left click target
+  target.addEventListener("click", (event) => {
     event.stopPropagation();
 
     targetLogic(target);
   });
 
-  // Right click support
+  // Right click target
   target.addEventListener("contextmenu", (event) => {
+    event.stopPropagation();
+
     event.preventDefault();
 
-    targetLogic(event, target);
+    targetLogic(target);
   });
+
+  // target.addEventListener("mousedown", targetLogic);
+  // target.addEventListener("contextmenu", targetLogic);
 }
 
-function innerTargetHTML(currentPoints, losePoints) {
+function innerTargetHTML(currentPoints, lostPoints) {
   return `
     <span class="target--current-points">
       +${currentPoints}
     </span>
     <span class="target--lose-points">
-      -${losePoints}
+      -${lostPoints}
     </span> 
     `;
 }
 
-// Click and right click support for Missed targets
-
-gameEl.addEventListener("mousedown", (e) => missedTargetLogic(e));
-gameEl.addEventListener("contextmenu", (e) => {
-  e.preventDefault();
-  missedTargetLogic(e);
-});
-
 function missedTargetLogic(e) {
+  e.preventDefault();
+
+  console.log(`🗑️🗑️🗑️🗑️🗑️`);
+
+  missedTargetsCounter++;
+  missEl.innerText = missedTargetsCounter;
+
   createMissedTarget(e.offsetX, e.offsetY);
-  countMisses();
 }
 
 function createMissedTarget(x, y) {
@@ -171,43 +330,12 @@ function createMissedTarget(x, y) {
   missEl.className = "miss";
   missEl.id = randomId();
 
-  gameEl.appendChild(missEl);
+  boardEl.appendChild(missEl);
 
+  // -- Auto-remove after 10s
   setTimeout(() => {
-    gameEl.removeChild(missEl);
+    boardEl.removeChild(missEl);
   }, 10000);
-}
-
-function countMisses(points) {
-  missEl.innerText = ++missCounter;
-
-  if (score - points > 0) {
-    updateScore(-points);
-
-    updateScoreStatus("substract", points);
-  } else if (score - points <= 0) {
-    updateScore(0);
-  }
-}
-
-// Updates every n millisecons
-
-respawnTargetInterval = setInterval(checkNumberOfTargets, targetRespawnTime);
-
-function checkNumberOfTargets() {
-  const numberOfTargets = gameEl.querySelectorAll(".target").length;
-
-  if (numberOfTargets < numberOfTargetsLimit)
-    createTarget(
-      randomCoordinate(gameEl.offsetWidth),
-      randomCoordinate(gameEl.offsetHeight)
-    );
-
-  const targetsArray = gameEl.querySelectorAll(".target");
-  for (let i = 0; i < targetsArray.length; i++) {
-    changeTargetWidth(targetsArray[i]);
-    moveTargetOnTheBoard(targetsArray[i]);
-  }
 }
 
 function changeTargetWidth(target) {
@@ -218,31 +346,18 @@ function changeTargetWidth(target) {
 }
 
 function moveTargetOnTheBoard(target) {
-  if (target.offsetWidth + target.offsetLeft > gameEl.offsetWidth) {
-    target.style.left = gameEl.offsetWidth - target.offsetWidth + "px";
+  if (target.offsetWidth + target.offsetLeft > boardEl.offsetWidth) {
+    target.style.left = boardEl.offsetWidth - target.offsetWidth + "px";
   }
-  if (target.offsetHeight + target.offsetTop > gameEl.offsetHeight) {
-    target.style.top = gameEl.offsetHeight - target.offsetHeight + "px";
+  if (target.offsetHeight + target.offsetTop > boardEl.offsetHeight) {
+    target.style.top = boardEl.offsetHeight - target.offsetHeight + "px";
   }
 }
-
-// Increment/Decrement feature
-
-targetsCurrentValueEl.addEventListener("input", changeTargetsNumber);
 
 function changeTargetsNumber() {
   numberOfTargetsLimit = targetsCurrentValueEl.value;
   numberOfTargetsEl.innerText = numberOfTargetsLimit;
 }
-
-// Respawn target feature
-
-respawnValueEl.min = 1;
-respawnValueEl.max = 2000;
-respawnValueEl.value = targetRespawnTime;
-respawnCurrentValueEl.innerText = targetRespawnTime;
-
-respawnValueEl.addEventListener("input", changeRespawnTime);
 
 function changeRespawnTime() {
   targetRespawnTime = respawnValueEl.value;
@@ -251,30 +366,12 @@ function changeRespawnTime() {
 
   clearInterval(respawnTargetInterval);
 
-  respawnTargetInterval = setInterval(checkNumberOfTargets, targetRespawnTime);
+  if (startGame) {
+    respawnTargetInterval = setInterval(checkTargets, targetRespawnTime);
+  }
 }
-
-// target size
-
-targetSizeEl.min = 20;
-targetSizeEl.max = 120;
-targetSizeEl.step = 1;
-targetSizeEl.value = targetWidth;
-
-targetSizeEl.addEventListener("input", setTargetWidth);
-
-const currentSizeValueEl = document.getElementById("size--current-value");
-currentSizeValueEl.innerText = targetWidth + " px";
 
 function setTargetWidth() {
   targetWidth = targetSizeEl.value;
-  currentSizeValueEl.innerText = targetWidth + " px";
+  currentSizeValueEl.innerText = targetWidth;
 }
-
-// toggle menu
-const navbarEl = document.querySelector(".navbar");
-const btnToggleMenu = document.querySelector(".btn-toggle-menu");
-
-btnToggleMenu.addEventListener("click", () => {
-  navbarEl.classList.toggle("open-menu");
-});
